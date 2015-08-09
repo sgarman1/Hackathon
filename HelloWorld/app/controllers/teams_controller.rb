bf049e4b0644c8e81ac3745a91ee26a46bc29788
sgarman11 <steven.garman@workday.com>
2015-08-09 06:20:00 +0000

    Second commit with Team changes

--- HelloWorld/app/controllers/teams_controller.rb
+++ HelloWorld/app/controllers/teams_controller.rb
@@ -69,6 +69,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:team_name, :Users, :username, :Project, :project_name, :Task, :task_name)
+      params.require(:team).permit(:team_name)
     end
 end
